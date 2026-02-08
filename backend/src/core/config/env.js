12d3d38d4a365aa94eb6bd86349d0afdd31f90dc
Sk Nooruddin <nooruddinsk660@gmail.com>
2026-02-08 10:39:33 +0530

    updated env.js

--- backend/src/core/config/env.js
+++ backend/src/core/config/env.js
@@ -54,6 +54,7 @@ const config = {
     cookie: {
       secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
   },
